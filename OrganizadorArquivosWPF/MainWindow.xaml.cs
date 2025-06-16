2bdd74d4b9152293cf74ef94d8541ae7ec42c0c6
loboczss <juanmproducoes@gmail.com>
2025-06-16 08:09:27 -0500

    ok

--- OrganizadorArquivosWPF/MainWindow.xaml.cs
+++ OrganizadorArquivosWPF/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using Microsoft.Win32;
+using System.Linq;
 using Ookii.Dialogs.Wpf;
 using OrganizadorArquivosWPF.Models;
 using OrganizadorArquivosWPF.Services;
